9dec13fdca5e584f4996ba9aff917f455d73007c
Bruno C Pires <bcpires451@gmail.com>
2024-01-08 20:55:36 +0100

    reiniciar

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function adicionar(){
     let amigo = document.getElementById("nome-amigo");
     let lista = document.getElementById("lista-amigos");
     amigos.push(amigo.value);
-    
+
     if (lista.textContent == "") {
         lista.textContent = amigo.value;
     }
@@ -40,4 +40,10 @@ function embaralha(lista) {
         [lista[indice - 1], lista[indiceAleatorio]] = 
             [lista[indiceAleatorio], lista[indice - 1]];
     }
+}
+
+function reiniciar(){
+    amigos = [];
+    document.getElementById("lista-amigos").innerHTML = "";
+    document.getElementById("lista-sorteio").innerHTML = "";
 }
