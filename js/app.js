7b5c88beabc7b96ab3689e1ed713a6858d3cdc16
Bruno C Pires <bcpires451@gmail.com>
2024-01-09 12:38:44 +0100

    comentarios

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ let amigos = [];
 
 function adicionar(){
     let amigo = document.getElementById("nome-amigo");
+    if(amigo.value == ""){
+        alert("Informe o nome do amigo");
+        return; //para encerrar a funçao e retornar à primeira linha.
+    }
     let lista = document.getElementById("lista-amigos");
     amigos.push(amigo.value);
 
@@ -14,19 +18,19 @@ function adicionar(){
     amigo.value = "";
 }
 
-function sortear(){
+function sortear(){//nao esta funcionando, verificar esta e a funçao embaralha.
     embaralha(amigos);
 
     let sorteio = document.getElementById("lista-sorteio");
 
-    for (let i = 0; i < amigos.lenght; i++){
+    for (let i = 0; i < amigos.lenght; i++){//i é uma variavel que parte de 0; deve ser menor que a lista de amigos; vai somando 1 para cada amigo)
         
         if(i == amigos.lenght - 1){
             sorteio.innerHTML = sorteio.innerHTML + amigos[i] + "-->" + amigos[0] + "<br/>"
-        }
+        }// se chegar ao ultimo amigo, selecionar o primeiro=0, o ultimo amigo vai tirar o primeiro no amigo secreto.
         else{
             sorteio.innerHTML = sorteio.innerHTML + amigos[i] + "-->" + amigos[i + 1] + "<br/>"
-        }
+        }//o o primeiro amigo vi tirar o segundo, que vai tirar o terceiro, e cosi via.
     }  
 }
 
