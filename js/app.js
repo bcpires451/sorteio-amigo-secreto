f9295004f810d2aea6de17b50f0feccba276c27b
Bruno C Pires <bcpires451@gmail.com>
2024-01-09 16:55:34 +0100

    projeto final resolvido

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function adicionar(){
     }
     if(amigos.includes(amigo.value)){
         alert("Nome ja adicionado");
-        return;
+        return
     }
     let lista = document.getElementById("lista-amigos");
     amigos.push(amigo.value);
@@ -22,24 +22,36 @@ function adicionar(){
     amigo.value = "";
 }
 
-function sortear() {
-    if (amigos.length < 4) {
-        alert("Adicione pelo menos 4 amigos!");
-        return;
-    }
+function sortear(){//nao esta funcionando, verificar esta e a funçao embaralha.
+    embaralha(amigos);
 
-    embaralhar(amigos);
+    let sorteio = document.getElementById("lista-sorteio");
 
-    let sorteio = document.getElementById('lista-sorteio');
-    for (let i = 0; i < amigos.length; i++) {
-        if (i == amigos.length - 1) {
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +" --> " +amigos[0] + "<br/>";
-        } else {
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +" --> " +amigos[i + 1] + "<br/>";
+    for (let i = 0; i < amigos.length; i++){//i é uma variavel que parte de 0; deve ser menor que a lista de amigos; vai somando 1 para cada amigo)
+        
+        if(amigos.length < 4){
+            alert("Informe pelo menos 4 amigos");
+            return;
         }
-    }
+        else if(i == amigos.length - 1){
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + "-->" + amigos[0] + "<br/>";
+        }// o ultimo amigo vai tirar o primeiro no amigo secreto(0)
+        else{
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + "-->" + amigos[i + 1] + "<br/>";
+        }//o o primeiro amigo vai tirar o segundo, que vai tirar o terceiro, e via dicendo.
+    }  
 }
 
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        //atribuiçao via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
 
 function reiniciar(){
     amigos = [];
