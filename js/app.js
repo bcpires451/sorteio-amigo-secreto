41613b449783410533bd884ad86374ad78d3250d
Bruno C Pires <bcpires451@gmail.com>
2024-01-09 13:40:54 +0100

    projeto final

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ function adicionar(){
         alert("Informe o nome do amigo");
         return; //para encerrar a funçao e retornar à primeira linha.
     }
+    if(amigos.includes(amigo.value)){
+        alert("Nome ja adicionado");
+        return;
+    }
     let lista = document.getElementById("lista-amigos");
     amigos.push(amigo.value);
 
@@ -18,33 +22,24 @@ function adicionar(){
     amigo.value = "";
 }
 
-function sortear(){//nao esta funcionando, verificar esta e a funçao embaralha.
-    embaralha(amigos);
+function sortear() {
+    if (amigos.length < 4) {
+        alert("Adicione pelo menos 4 amigos!");
+        return;
+    }
 
-    let sorteio = document.getElementById("lista-sorteio");
+    embaralhar(amigos);
 
-    for (let i = 0; i < amigos.lenght; i++){//i é uma variavel que parte de 0; deve ser menor que a lista de amigos; vai somando 1 para cada amigo)
-        
-        if(i == amigos.lenght - 1){
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + "-->" + amigos[0] + "<br/>"
-        }// se chegar ao ultimo amigo, selecionar o primeiro=0, o ultimo amigo vai tirar o primeiro no amigo secreto.
-        else{
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + "-->" + amigos[i + 1] + "<br/>"
-        }//o o primeiro amigo vi tirar o segundo, que vai tirar o terceiro, e cosi via.
-    }  
-}
-
-
-function embaralha(lista) {
-
-    for (let indice = lista.lenght; indice; indice--) {
-        const indiceAleatorio = Math.floor(Math.random() * indice);
-
-        //atribuiçao via destructuring
-        [lista[indice - 1], lista[indiceAleatorio]] = 
-            [lista[indiceAleatorio], lista[indice - 1]];
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +" --> " +amigos[0] + "<br/>";
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +" --> " +amigos[i + 1] + "<br/>";
+        }
     }
 }
+
 
 function reiniciar(){
     amigos = [];
